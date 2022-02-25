8c62788910e1ab9a302745521f57bc495c4bca7d
Brooke <belv2c@gmail.com>
2022-02-25 14:55:20 -0600

    added initial button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,12 @@
-import logo from "../../assets/logo.svg"
+import { Button } from "../Button"
 import "./App.styles.css"
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/App/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button ariaLabel="example button" name="example button">
+        Button text
+      </Button>
     </div>
   )
 }
---
+++ src/components/Button/README.md
@@ -0,0 +1,21 @@
+# `<Button />`
+
+DESCRIPTION_HERE
+
+## Directory Structure
+
+- `Button.stories.tsx`: Component stories (`npm run test:playground`)
+- `Button.test.tsx`: Component tests (`npm run test:unit`)
+- `Button.tsx`: Component code
+- `index.ts`: Component export
+- `README.md`: Component documentation (hey, that's me!)
+
+## Example
+
+![Button](../../../.loki/reference/chrome_Button_example.png)
+
+> Image location: [`.loki/reference/chrome_Button_example.png`](../../../.loki/reference/chrome_Button_example.png)
+> 
+> To regenerate: 
+> 1. `npm run test:playground` (skip if running)
+> 1. `npm run test:visual:update -- --storiesFilter="^Button example\$"`
