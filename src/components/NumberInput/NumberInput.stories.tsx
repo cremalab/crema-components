b80b0bc6a1229ad8a0ed94f39b09c4739e9c6db0
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-01 10:11:49 -0500

    working through stories

---
+++ src/components/NumberInput/NumberInput.stories.tsx
@@ -0,0 +1,63 @@
+import decoratorCentered from "@storybook/addon-centered"
+import { Story } from "@storybook/react"
+import { ComponentProps } from "react"
+import { NumberInput } from "./NumberInput"
+import "./storyStyles.css"
+
+/**
+ * See Storybook Docs: Writing Stories
+ * https://storybook.js.org/docs/basics/writing-stories/
+ */
+
+export default {
+  title: "NumberInput",
+  decorators: [decoratorCentered],
+}
+
+const Template: Story<ComponentProps<typeof NumberInput>> = (args) => (
+  <NumberInput {...args} />
+)
+
+export const Default = Template.bind({})
+
+Default.args = {
+  control: (props) => <input {...props} />,
+  min: 4,
+  max: 100,
+}
+
+export const CustomButton = Template.bind({})
+
+CustomButton.args = {
+  control: (props) => <input {...props} />,
+  min: 4,
+  max: 100,
+  customButton: (props) => <button {...props} className="customButton" />,
+}
+
+export const StyledContainer = Template.bind({})
+
+StyledContainer.args = {
+  control: (props) => <input {...props} />,
+  min: 4,
+  max: 100,
+  containerClassName: "container",
+}
+
+export const StyledInput = Template.bind({})
+
+StyledInput.args = {
+  control: (props) => <input {...props} className="customInput" />,
+}
+
+export const StyledEverything = Template.bind({})
+
+StyledEverything.args = {
+  containerClassName: "container centered",
+  control: (props) => <input {...props} className="customInput fullWidth" />,
+  customButton: (props) => (
+    <button {...props} className="customButton fullWidth" />
+  ),
+  incrementText: "Add",
+  decrementText: "Subtract",
+}
