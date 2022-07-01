c22c5a3d51513bce420dc69410db77209e14f779
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-01 11:46:30 -0500

    added case for decrease

--- src/components/NumberInput/NumberInput.test.tsx
+++ src/components/NumberInput/NumberInput.test.tsx
@@ -10,7 +10,7 @@ describe("NumberInput", () => {
     // Act
     render(
       <NumberInput
-        min={0}
+        defaultValue={0}
         control={(props) => <input data-testid={testID} {...props} />}
       />,
     )
@@ -21,5 +21,24 @@ describe("NumberInput", () => {
 
     // Assert
     expect(input.value).toBe("1")
+  })
+  it("decreases the value when increment action is invoked", async () => {
+    // Arrange
+    const decreaseLabel = "click to decrease value"
+    const testID = "numberInput"
+    // Act
+    render(
+      <NumberInput
+        defaultValue={0}
+        control={(props) => <input data-testid={testID} {...props} />}
+      />,
+    )
+
+    const decreaseButton = screen.getByLabelText(decreaseLabel)
+    await userEvent.click(decreaseButton)
+    const input = screen.getByTestId(testID) as HTMLInputElement
+
+    // Assert
+    expect(input.value).toBe("-1")
   })
 })
