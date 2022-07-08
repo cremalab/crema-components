e3710e33d0dd732f93f8367c791d9680e8ab5b70
Brooke <belv2c@gmail.com>
2022-07-08 16:23:26 -0500

    feat: added more storybook stories for modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,7 +1,7 @@
-import decoratorCentered from "@storybook/addon-centered"
 import { ComponentMeta, Story } from "@storybook/react"
-import { ComponentProps } from "react"
+import { ComponentProps, useState } from "react"
 import { Button } from "../Button"
+import { TextInput } from "../TextInput"
 import { Modal } from "./Modal"
 
 /**
@@ -12,29 +12,89 @@ import { Modal } from "./Modal"
 export default {
   title: "Components/Modal",
   component: Modal,
-  decorators: [decoratorCentered],
 } as ComponentMeta<typeof Modal>
 
-const Template: Story<ComponentProps<typeof Modal>> = (args) => {
-  return <Modal {...args} />
+export const Basic: Story<ComponentProps<typeof Modal>> = (args) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div>
+      <Button ariaLabel="Open" onClick={() => setIsOpen(true)}>
+        Open basic modal
+      </Button>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)} {...args}>
+        <p>I am a basic modal 😃</p>
+        <Button
+          ariaLabel="Close Modal"
+          aria-labelledby="close-modal"
+          className="close-modal"
+          onClick={() => setIsOpen(false)}
+        >
+          Close
+        </Button>
+      </Modal>
+    </div>
+  )
 }
 
-export const Default = Template.bind({})
+export const WithTitle: Story<ComponentProps<typeof Modal>> = (args) => {
+  const [isOpen, setIsOpen] = useState(false)
 
-Default.args = {
-  className: "modal-class",
-  open: true,
-  title: "Modal title",
-  role: "dialog",
-  children: (
-    <Button
-      ariaLabel="Close Modal"
-      aria-labelledby="close-modal"
-      className="close-modal-button"
-      onClick={() => undefined}
-    >
-      Close
-    </Button>
-  ),
-  "aria-label": "Modal",
+  return (
+    <div>
+      <Button ariaLabel="Open" onClick={() => setIsOpen(true)}>
+        Open modal with title
+      </Button>
+      <Modal
+        title="Modal With Title"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        {...args}
+      >
+        <p>I am a modal. I have a title! 😃</p>
+        <Button
+          ariaLabel="Close Modal"
+          aria-labelledby="close-modal"
+          className="close-modal"
+          onClick={() => setIsOpen(false)}
+        >
+          Close
+        </Button>
+      </Modal>
+    </div>
+  )
+}
+
+export const WithMoreContent: Story<ComponentProps<typeof Modal>> = (args) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div>
+      <Button ariaLabel="Open" onClick={() => setIsOpen(true)}>
+        Open modal with more content
+      </Button>
+      <Modal
+        title="Modal With Title"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        {...args}
+      >
+        <div>
+          <p>Modals can be cool</p>
+          <p>I am a modal. I have a title! 😃</p>
+          <TextInput name="test-input" label="Test Input" hideLabel={false} />
+          <div style={{ marginTop: "1rem" }}>
+            <Button
+              ariaLabel="Close Modal"
+              aria-labelledby="close-modal"
+              className="close-modal"
+              onClick={() => setIsOpen(false)}
+            >
+              Close
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  )
 }
