7f3b19844ca9531a1a4a6d78427af783f1dfbfc3
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-25 14:30:29 -0500

    finalized ToastContext

--- src/context/ToasterContext.tsx
+++ src/context/ToasterContext.tsx
@@ -3,111 +3,153 @@ import {
   createContext,
   useCallback,
   useEffect,
+  useRef,
   useState,
 } from "react"
+import { createPortal } from "react-dom"
 import { v4 } from "uuid"
+import { Transition, TransitionGroup } from "react-transition-group"
 import { Toast as ToastCard } from "../components/Toast"
+import { Position, Toast } from "../types/Toast"
+import { useKeyPress } from "../hooks/useKeyPress"
+import {
+  getToastPositionStyles,
+  getToastTransitionStyles,
+} from "../utils/toastUtils"
 
-interface Position {
-  vertical?: "top" | "center" | "bottom"
-  horizontal?: "left" | "center" | "right"
+interface Config {
+  duration: number
+  animationDuration: number
+  position: Position
+  behavior: "stack" | "replace"
 }
 
-export interface ToasterConfig {
-  duration?: number
-  animationDuration?: number
-  autoDismiss?: boolean
-  position?: Position
-}
+export class ToasterConfig {
+  private duration: Config["duration"]
+  private animationDuration: Config["animationDuration"]
+  private position: Config["position"]
+  private behavior: Config["behavior"]
+  constructor(config: Partial<Config> = {}) {
+    this.duration = config.duration || 5000
+    this.animationDuration = config.animationDuration || 300
+    this.position = config.position || {
+      vertical: "bottom",
+      horizontal: "center",
+    }
+    this.behavior = config.behavior || "stack"
+  }
 
-export const createToasterConfig = (config: ToasterConfig) => config
+  getConfig() {
+    return {
+      duration: this.duration,
+      animationDuration: this.animationDuration,
+      position: this.position,
+      behavior: this.behavior,
+    }
+  }
+}
 
 interface ToasterProviderProps {
   children: ReactNode
-  config?: ToasterConfig
-}
-
-type ToastType = "success" | "info" | "error" | "warning"
-
-type Toast = {
-  id: string
-  message: string
-  type: ToastType
-  onClose?: () => void
-  action?: ReactNode
+  config: ToasterConfig
 }
 
 interface ContextProps {
   toasts: Toast[]
-  addToast: (notification: Omit<Toast, "id">) => void
+  addToast: (toast: Omit<Toast, "id">) => void
+  removeAll: () => void
 }
 
 export const ToasterContext = createContext<ContextProps>({
   toasts: [],
-  addToast: (notification) => [notification],
+  addToast: () => null,
+  removeAll: () => null,
 })
 
-const DEFAULT_POSITION: Position = {
-  vertical: "bottom",
-  horizontal: "center",
-}
-
-const DEFAULT_DURATION = 5000
-
-const DEFAULT_ANIMATION_DURATION = 300
-
-export function ToasterProvider({
-  children,
-  config = {},
-}: ToasterProviderProps) {
-  const {
-    duration = DEFAULT_DURATION,
-    autoDismiss = true,
-    position = DEFAULT_POSITION,
-    animationDuration = DEFAULT_ANIMATION_DURATION,
-  } = config
-
-  const id = v4()
-
+export function ToasterProvider({ children, config }: ToasterProviderProps) {
+  const { animationDuration, duration, behavior, position } = config.getConfig()
   const [toasts, setToasts] = useState<Toast[]>([])
+  const timer = useRef<NodeJS.Timeout>()
+
+  const removeToast = useCallback((id: string) => {
+    setToasts((toasts) => toasts.filter((toast) => toast.id !== id))
+  }, [])
+
+  const removeOldest = useCallback(() => {
+    setToasts((toasts) => toasts.slice(1))
+  }, [])
+
+  useKeyPress(["Escape"], removeOldest)
 
   useEffect(() => {
-    const timer = setTimeout(
-      () => setToasts(toasts.slice(1)),
-      duration + DEFAULT_ANIMATION_DURATION,
-    )
+    timer.current = setTimeout(removeOldest, duration)
     return () => {
-      clearTimeout(timer)
+      clearTimeout(timer.current)
     }
-  }, [toasts, duration])
+  }, [toasts, duration, removeOldest])
 
   const addToast: ContextProps["addToast"] = useCallback(
-    (notification) =>
-      setToasts((toasts) => [{ ...notification, id }, ...toasts]),
-    [id],
+    (toast) => {
+      const newToast: Toast = { ...toast, id: v4() }
+      if (behavior === "stack") {
+        if (position.vertical === "center" || position.vertical === "top") {
+          setToasts((toasts) => [newToast, ...toasts])
+        } else {
+          setToasts((toasts) => [...toasts, newToast])
+        }
+      } else {
+        setToasts([newToast])
+      }
+    },
+    [behavior, position],
   )
 
+  const removeAll = () => {
+    setToasts([])
+  }
+
   return (
-    <ToasterContext.Provider value={{ toasts, addToast }}>
+    <ToasterContext.Provider value={{ toasts, addToast, removeAll }}>
       {children}
-      <div>
-        {toasts.map((notification) => (
-          <div key={notification.id}>
-            <ToastCard
-              showToast={true}
-              autoDismiss={autoDismiss}
-              duration={duration}
-              animationDuration={animationDuration}
-              horizontalPosition={position.horizontal}
-              verticalPosition={position.vertical}
-              handleClose={notification.onClose}
-              message={notification.message}
-              action={notification.action}
-              type={notification.type}
-            />
+      {createPortal(
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div style={getToastPositionStyles({ position })}>
+            <TransitionGroup
+              style={{ display: "flex", gap: "8px 0", flexDirection: "column" }}
+            >
+              {toasts.map((toast) => (
+                <Transition
+                  unmountOnExit
+                  key={toast.id}
+                  timeout={animationDuration}
+                >
+                  {(state) => (
+                    <div
+                      style={getToastTransitionStyles({
+                        animationDuration,
+                        transitionStatus: state,
+                      })}
+                    >
+                      <ToastCard
+                        message={toast.message}
+                        action={toast.action?.(() => removeToast(toast.id))}
+                        status={toast.status}
+                      />
+                    </div>
+                  )}
+                </Transition>
+              ))}
+            </TransitionGroup>
           </div>
-        ))}
-      </div>
+        </div>,
+        document.body,
+      )}
     </ToasterContext.Provider>
   )
 }
