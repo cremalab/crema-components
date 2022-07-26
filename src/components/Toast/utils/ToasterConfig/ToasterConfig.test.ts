55fe5d29cff0851f5abef8fddaff142b6c7d5ce1
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-07-26 10:08:17 -0500

    fixed test and added second case

--- src/components/Toast/utils/ToasterConfig/ToasterConfig.test.ts
+++ src/components/Toast/utils/ToasterConfig/ToasterConfig.test.ts
@@ -3,18 +3,39 @@ import { ToasterConfig } from "./ToasterConfig"
 describe("ToasterConfig", () => {
   it("getConfig returns a default object when no params are passed", () => {
     // Arrange
-    const { getConfig } = new ToasterConfig()
+    const toasterConfig = new ToasterConfig()
 
     // Act
-    const received = getConfig()
+    const received = toasterConfig.getConfig()
     const expected = {
       duration: 5000,
       animationDuration: 300,
       position: {
-        vertical: "center",
+        vertical: "bottom",
         horizontal: "center",
       },
       behavior: "stack",
+    }
+
+    // Assert
+    expect(received).toEqual(expected)
+  })
+  it("getConfig returns an object based on params that are passed", () => {
+    // Arrange
+    const toasterConfig = new ToasterConfig({
+      animationDuration: 400,
+      duration: 300,
+      behavior: "replace",
+      position: { horizontal: "left", vertical: "center" },
+    })
+
+    // Act
+    const received = toasterConfig.getConfig()
+    const expected = {
+      animationDuration: 400,
+      duration: 300,
+      behavior: "replace",
+      position: { horizontal: "left", vertical: "center" },
     }
 
     // Assert
