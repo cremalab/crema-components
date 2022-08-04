0e715e52d9d85a99d07a9c99a9138bdbb1cffdff
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-04 13:57:30 -0500

    initial implementation and stories, no tests

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,4 +1,5 @@
-import decoratorCentered from "@storybook/addon-centered"
+import decoratedCenter from "@storybook/addon-centered"
+import { ComponentStory } from "@storybook/react"
 import { Tooltip } from "./Tooltip"
 
 /**
@@ -6,9 +7,117 @@ import { Tooltip } from "./Tooltip"
  * https://storybook.js.org/docs/basics/writing-stories/
  */
 
+const basePlacement = ["top", "bottom", "left", "right", "auto"]
+const startPlacement = basePlacement.map((placement) => `${placement}-start`)
+const endPlacement = basePlacement.map((placement) => `${placement}-end`)
+
 export default {
-  title: "Tooltip",
-  decorators: [decoratorCentered],
+  title: "Components/Tooltip",
+  decorators: [decoratedCenter],
+  argTypes: {
+    placement: {
+      control: {
+        type: "select",
+        options: [...basePlacement, ...startPlacement, ...endPlacement],
+      },
+    },
+  },
 }
 
-export const Example = () => <Tooltip />
+const Template: ComponentStory<typeof Tooltip> = (args) => {
+  return <Tooltip {...args} />
+}
+
+export const Default = Template.bind({})
+
+Default.args = {
+  children: "Hello",
+  label: "World",
+  horizontalOffset: 10,
+  verticalOffset: 0,
+  placement: "bottom",
+  showArrow: false,
+  enterDelay: 0,
+  exitDelay: 0,
+}
+
+export const WithArrow = Template.bind({})
+
+WithArrow.args = {
+  ...Default.args,
+  alwaysShow: true,
+  showArrow: true,
+}
+
+export const WithoutArrow = Template.bind({})
+
+WithoutArrow.args = {
+  ...Default.args,
+  alwaysShow: true,
+}
+
+export const WithAdditionalContent: ComponentStory<typeof Tooltip> = (args) => {
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          in ipsum id orci porta dapibus.
+        </p>
+      </div>
+      <div>
+        <p>Some text above.</p>
+        <Tooltip {...args} />
+        <p>Some text below.</p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          in ipsum id orci porta dapibus.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+WithAdditionalContent.args = {
+  ...Default.args,
+  showArrow: true,
+  placement: "bottom",
+  alwaysShow: false,
+}
+
+export const PlacementTop = Template.bind({})
+
+PlacementTop.args = {
+  ...Default.args,
+  placement: "top",
+  alwaysShow: true,
+  showArrow: true,
+}
+export const PlacementRight = Template.bind({})
+
+PlacementRight.args = {
+  ...Default.args,
+  placement: "right",
+  alwaysShow: true,
+  showArrow: true,
+}
+
+export const PlacementBottom = Template.bind({})
+
+PlacementBottom.args = {
+  ...Default.args,
+  placement: "bottom",
+  alwaysShow: true,
+  showArrow: true,
+}
+
+export const PlacementLeft = Template.bind({})
+
+PlacementLeft.args = {
+  ...Default.args,
+  placement: "left",
+  alwaysShow: true,
+  showArrow: true,
+}
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,3 +1,87 @@
-export function Tooltip() {
-  return <></>
+import { Placement } from "@popperjs/core"
+import { ReactNode, useEffect, useRef, useState } from "react"
+import { usePopper } from "react-popper"
+import classes from "./Tooltip.module.css"
+
+interface TooltipProps {
+  children: ReactNode
+  label: string
+  alwaysShow?: boolean
+  showArrow?: boolean
+  horizontalOffset?: number
+  verticalOffset?: number
+  placement?: Placement
+  enterDelay?: number
+  exitDelay?: number
+}
+
+export function Tooltip({
+  children,
+  label,
+  showArrow,
+  alwaysShow,
+  placement = "auto",
+  horizontalOffset = 10,
+  verticalOffset = 0,
+  enterDelay,
+  exitDelay,
+}: TooltipProps) {
+  const timer = useRef<NodeJS.Timeout>()
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current)
+    }
+  }, [])
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [anchorElement, setAnchorElement] = useState<HTMLSpanElement | null>(
+    null,
+  )
+  const boxRef = useRef<HTMLDivElement>(null)
+  const arrowRef = useRef<HTMLDivElement>(null)
+
+  const { styles, attributes } = usePopper(anchorElement, boxRef.current, {
+    modifiers: [
+      { name: "arrow", options: { element: arrowRef.current } },
+      {
+        name: "offset",
+        options: { offset: [verticalOffset, horizontalOffset] },
+      },
+    ],
+    placement,
+    strategy: "fixed",
+  })
+
+  const handleOpen = () => {
+    timer.current = setTimeout(() => setIsOpen(true), enterDelay ?? 0)
+  }
+
+  const handleClose = () => {
+    timer.current = setTimeout(() => setIsOpen(false), exitDelay ?? 0)
+  }
+
+  return (
+    <div>
+      <span
+        onMouseEnter={handleOpen}
+        onMouseLeave={handleClose}
+        ref={setAnchorElement}
+      >
+        {children}
+      </span>
+      <div
+        ref={boxRef}
+        style={styles.popper}
+        data-is-open={isOpen || alwaysShow ? "true" : "false"}
+        className={classes.box}
+        {...attributes.popper}
+      >
+        {label}
+        {showArrow && (
+          <div ref={arrowRef} className={classes.arrow} style={styles.arrow} />
+        )}
+      </div>
+    </div>
+  )
 }
