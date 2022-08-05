2d3c3400a437d3e64eb779562dd73d608d1efc36
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-05 09:40:31 -0500

    two test cases passing

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -1,7 +1,42 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { Tooltip } from "./Tooltip"
 
 describe("Tooltip", () => {
-  it("is defined", expect(Tooltip).toBeDefined)
+  it("displays tooltip onMouseEnter", async () => {
+    // Arrange
+    const children = "Hello"
+    const label = "World"
 
-  it.todo(`add meaningful tests 👍`)
+    // Act
+    render(<Tooltip label={label}>{children}</Tooltip>)
+
+    const receivedChild = screen.getByText(children)
+
+    expect(screen.getByText(label)).not.toBeVisible()
+
+    await userEvent.hover(receivedChild)
+
+    // Assert
+    expect(screen.getByText(label)).toBeVisible()
+  })
+  it("hides tooltip onMouseLeave", async () => {
+    // Arrange
+    const children = "Hello"
+    const label = "World"
+
+    // Act
+    render(<Tooltip label={label}>{children}</Tooltip>)
+
+    const receivedChild = screen.getByText(children)
+
+    await userEvent.hover(receivedChild)
+
+    expect(screen.getByText(label)).toBeVisible()
+
+    await userEvent.unhover(receivedChild)
+
+    // Assert
+    expect(screen.getByText(label)).not.toBeVisible()
+  })
 })
