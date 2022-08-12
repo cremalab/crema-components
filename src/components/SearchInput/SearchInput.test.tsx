3e72999bd25c8a7429fed47a7e7f6aa37a0331fb
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-12 12:55:52 -0500

    working through first test case

--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,7 +1,7 @@
 import { SearchInput } from "./SearchInput"
 
 describe("SearchInput", () => {
-  it("is defined", expect(SearchInput).toBeDefined)
+  it("debounces and invokes a callback", expect(SearchInput).toBeDefined)
 
   it.todo(`add meaningful tests 👍`)
 })
