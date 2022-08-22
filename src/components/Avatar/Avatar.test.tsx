8e29344de9d1e3b73b5f5e56573c6a236c50e830
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-22 14:05:37 -0500

    adding more test cases

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -16,4 +16,33 @@ describe("Avatar", () => {
     // Assert
     expect(received).toBeInTheDocument()
   })
+  it("does not render initials if image is supplied", () => {
+    // Arrange
+    const name = "Crema Components"
+    const initials = getInitials(name)
+
+    // Act
+    render(<Avatar name={name} src="someUrl" />)
+
+    const received = screen.queryByText(initials)
+
+    // Assert
+    expect(received).not.toBeInTheDocument()
+  })
+  it("renders a meaningful aria label when an image is supplied", () => {
+    // Arrange
+    const name = "Crema Components"
+    const initials = getInitials(name)
+    const ariaLabel = `An image of ${name}`
+
+    // Act
+    render(<Avatar name={name} src="someUrl" />)
+
+    const received = screen.queryByText(initials)
+    const label = screen.getByLabelText(ariaLabel)
+
+    // Assert
+    expect(received).not.toBeInTheDocument()
+    expect(label).toBeInTheDocument()
+  })
 })
