5b0b37fb2c025445ce06badfe107d1910ae40c3e
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-22 14:01:51 -0500

    added var for ratio

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,7 +1,19 @@
+import { render, screen } from "@testing-library/react"
 import { Avatar } from "./Avatar"
+import { getInitials } from "./utils/getInitials"
 
 describe("Avatar", () => {
-  it("is defined", expect(Avatar).toBeDefined)
+  it("renders an avatar with initials", () => {
+    // Arrange
+    const name = "Crema Components"
+    const initials = getInitials(name)
 
-  it.todo(`add meaningful tests 👍`)
+    // Act
+    render(<Avatar name={name} />)
+
+    const received = screen.getByText(initials)
+
+    // Assert
+    expect(received).toBeInTheDocument()
+  })
 })
