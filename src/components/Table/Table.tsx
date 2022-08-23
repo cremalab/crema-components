5523ecbf25a5e47297b0516efc6b17ab7ae0c2c1
Rob LaFeve <roblafeve@gmail.com>
2022-08-23 11:58:36 -0500

    45: Reduce nested `if` statements

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -23,20 +23,17 @@ export function Table<Datum extends WithID>(props: Props<Datum>) {
   const data = [...dataUnsorted].sort((a, b) => {
     const valueA = sortConfig?.getValue(a)
     const valueB = sortConfig?.getValue(b)
-    if (valueA && valueB) {
-      if (valueA > valueB) return sortAsc ? 1 : -1
-      if (valueA < valueB) return sortAsc ? -1 : 1
-    }
+    if (valueA === undefined || valueB === undefined) return 0
+    if (valueA > valueB) return sortAsc ? 1 : -1
+    if (valueA < valueB) return sortAsc ? -1 : 1
     return 0
   })
 
   const handleSort = (columnConfig: ColumnConfig<Datum>) => () => {
-    if (columnConfig.sortable) {
-      if (columnConfig === sortConfig || !sortConfig) {
-        setSortAsc((value) => !value)
-      }
-      setSortConfig(columnConfig)
-    }
+    if (!columnConfig.sortable) return
+    if (columnConfig === sortConfig || !sortConfig)
+      setSortAsc((value) => !value)
+    setSortConfig(columnConfig)
   }
 
   const headers = columnConfigs.map((columnConfig, i) => {
