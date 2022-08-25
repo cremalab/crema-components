3660242a2c0d5771cc167bd5e6663e4487e92ba6
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2022-08-25 10:27:51 -0500

    conditionally adding backgroundImage

--- src/components/Avatar/AvatarBase.tsx
+++ src/components/Avatar/AvatarBase.tsx
@@ -24,7 +24,7 @@ export function AvatarBase({
     <div
       aria-label={ariaLabel}
       style={{
-        backgroundImage: `url(${src})`,
+        ...(src && { backgroundImage: `url(${src})` }),
         color: fontColor,
         backgroundColor,
       }}
